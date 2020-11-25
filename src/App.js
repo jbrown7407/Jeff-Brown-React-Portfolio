0e22e0e5af488932a03fd0d30d3bfa6fd496f8a1
jbrown7407 <jbrown7407@gmail.com>
2020-11-25 15:53:17 -0500

    Components for About, Projects, Articles
    Add components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 // import Hello from "./components/sayHello.jsx";
-// import Component1 from "./components/component1.jsx";
+
 import React, { useState } from "react";
 import Slide from "./components/Slide.jsx";
 import Header from "./components/Header.jsx";
 import About from "./components/About.jsx";
 import Articles from "./components/Articles.jsx";
 import Projects from "./components/Projects.jsx";
+import Component1 from "./components/component1.jsx";
 // import Hello from "./components/SayHello.jsx";
 
 function App() {
@@ -23,15 +24,28 @@ function App() {
 
   return (
     <div>
-      <h1 className={isRed ? "red" : "green"}>
-        {/* <-- add correlating CSS --> */}
-      </h1>
+      <div className="navigation">
+        <img src={logo} className="logo" alt="Logo Image" />
+        <div className="navigation">
+          <div className="navigation-sub">
+            <a href="" className="item">
+              Projects
+            </a>{" "}
+            |
+            <a href="" className="item">
+              Articles
+            </a>{" "}
+            |
+            <a href="" className="item">
+              About
+            </a>
+          </div>
+        </div>
+      </div>
       <Header></Header>
-      <About />
-      <Articles />
-      <Projects />
-      {/* <Hello></Hello>
-      <Component1></Component1> */}
+      <Projects></Projects>
+      <Articles></Articles>
+      <About></About>
       <Slide name="1" message="this is a Slide"></Slide>x
       <Slide name="2"></Slide>x<Slide name="3"></Slide>
       <button onClick={increment}> Increment </button>
