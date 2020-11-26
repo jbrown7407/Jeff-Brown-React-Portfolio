cfbc7fa0854c0c5af04beefcb23fb455c630ce96
jbrown7407 <jbrown7407@gmail.com>
2020-11-25 20:09:12 -0500

    spin react icon

--- src/App.js
+++ src/App.js
@@ -43,6 +43,20 @@ function App() {
         <Route path="/about" component={About} />
       </div>
       <CanvasAlpha> </CanvasAlpha>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </BrowserRouter>
   );
 }
